f0bf84d5a7f7df6be1d5b9187392bf599b915852
alidep <amdepsky@gmail.com>
2023-04-07 22:04:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,34 @@
-// Create a Three.js scene
-var scene = new THREE.Scene();
+import * as THREE from 'three';
 
-// Create a camera and add it to the scene
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-scene.add(camera);
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
-// Create a renderer and add it to the page
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 
-// Load the 3D model
-var loader = new THREE.OBJLoader();
+
+// instantiate a loader
+const loader = new OBJLoader();
+
+// load a resource
 loader.load(
-    'doggo.obj',
-    function (object) {
-        scene.add(object);
-    }
+	// resource URL
+	'mascot_test3.obj',
+	// called when resource is loaded
+	function ( object ) {
+
+		scene.add( object );
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
 );
-
-// Listen for mouse movement
-var mouse = new THREE.Vector2();
-document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-function onDocumentMouseMove(event) {
-    // Normalize mouse position
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    // Move the 3D model to follow the mouse
-    camera.position.x = mouse.x * 5;
-    camera.position.y = mouse.y * 5;
-    camera.lookAt(scene.position);
-}
-
-// Render the scene
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-animate();
+...
